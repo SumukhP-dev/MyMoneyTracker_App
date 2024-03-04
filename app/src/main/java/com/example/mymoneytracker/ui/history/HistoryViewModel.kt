8fe8a5f38fcc4d8dd1912b6bbd93d28595d3f515
SumukhP0 <spaspuleti3@gatech.edu>
2024-03-03 23:14:24 -0500

    Added History and Summary fragments

---
+++ app/src/main/java/com/example/mymoneytracker/ui/history/HistoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mymoneytracker.ui.history
+
+import androidx.lifecycle.ViewModel
+
+class HistoryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/mymoneytracker/ui/summary/SummaryViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mymoneytracker.ui.summary
+
+import androidx.lifecycle.ViewModel
+
+class SummaryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
