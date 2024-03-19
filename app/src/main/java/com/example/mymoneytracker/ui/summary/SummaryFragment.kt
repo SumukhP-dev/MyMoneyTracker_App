210cce1b775b27685e78aad6217549e12bbbada9
SumukhP0 <spaspuleti3@gatech.edu>
2024-03-18 20:05:58 -0400

    Added a progress bar on the summary fragment

--- app/src/main/java/com/example/mymoneytracker/ui/summary/SummaryFragment.kt
+++ app/src/main/java/com/example/mymoneytracker/ui/summary/SummaryFragment.kt
@@ -1,5 +1,12 @@
 package com.example.mymoneytracker.ui.summary
 
+import android.content.res.ColorStateList
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +14,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import com.example.mymoneytracker.R
@@ -39,49 +47,73 @@ class SummaryFragment : Fragment() {
         setFragmentResultListener("requestKey3") { requestKey, bundle ->
             netWorth = bundle.getInt("bundleKey3")
             displayTips(netWorth)
-            Log.d("issue1", "path1")
+            setProgress(netWorth)
         }
+
+        binding.progressBar.max = 100
+        binding.progressBar.progressTintList = ColorStateList.valueOf(Color.GREEN);
 
         return root
     }
 
+    // This function displays tips based on net worth
     private fun displayTips(netWorth: Int) {
-        Log.d("issue1", "path2")
-        if(netWorth > 100000) {
+        if(netWorth >= 100000) {
             binding.tipsText.text = getString(R.string.tipBase,
                 resources.getString(R.string.tip1),
                 resources.getString(R.string.tip6),
                 resources.getString(R.string.tip7),
                 resources.getString(R.string.tip8),
                 resources.getString(R.string.tip9))
-        } else if (netWorth > 10000) {
+        } else if (netWorth >= 10000) {
             binding.tipsText.text = getString(R.string.tipBase,
                 resources.getString(R.string.tip2),
                 resources.getString(R.string.tip6),
                 resources.getString(R.string.tip7),
                 resources.getString(R.string.tip8),
                 resources.getString(R.string.tip9))
-        } else if (netWorth > 1000) {
+        } else if (netWorth >= 1000) {
             binding.tipsText.text = getString(R.string.tipBase,
                 resources.getString(R.string.tip3),
                 resources.getString(R.string.tip6),
                 resources.getString(R.string.tip7),
                 resources.getString(R.string.tip8),
                 resources.getString(R.string.tip9))
-        } else if (netWorth > -1000) {
+        } else if (netWorth >= -1000) {
             binding.tipsText.text = getString(R.string.tipBase,
                 resources.getString(R.string.tip4),
                 resources.getString(R.string.tip6),
                 resources.getString(R.string.tip7),
                 resources.getString(R.string.tip8),
                 resources.getString(R.string.tip9))
-        } else if (netWorth > -10000) {
+        } else {
             binding.tipsText.text = getString(R.string.tipBase,
                 resources.getString(R.string.tip5),
                 resources.getString(R.string.tip6),
                 resources.getString(R.string.tip7),
                 resources.getString(R.string.tip8),
                 resources.getString(R.string.tip9))
+        }
+    }
+
+    // This function sets the amount of progress for the progress bar
+    private fun setProgress(netWorth: Int) {
+        binding.progressBar.progress = getProgress(netWorth)
+    }
+
+
+    // This function gets the amount of progress for the progress bar
+    private fun getProgress(netWorth: Int): Int {
+        if(netWorth >= 100000) {
+            return 100
+        } else if (netWorth >= 10000) {
+            return 80
+        } else if (netWorth >= 1000) {
+            return 60
+        } else if (netWorth >= -1000) {
+            return 40
+        } else {
+            return 20
         }
     }
 
