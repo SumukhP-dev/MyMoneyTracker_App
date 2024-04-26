d87144e9b858ded9b6af3512668b82e009fe9b78
SumukhP0 <spaspuleti3@gatech.edu>
2024-04-25 21:24:25 -0400

    Added error validation to AddDataFragment

--- app/src/main/java/com/example/mymoneytracker/ui/addData/AddDataFragment.kt
+++ app/src/main/java/com/example/mymoneytracker/ui/addData/AddDataFragment.kt
@@ -1,12 +1,15 @@
 package com.example.mymoneytracker.ui.addData
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
+import androidx.annotation.RequiresApi
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
@@ -15,6 +18,10 @@ import androidx.navigation.fragment.findNavController
 import com.example.mymoneytracker.MainActivity
 import com.example.mymoneytracker.R
 import com.example.mymoneytracker.databinding.FragmentAddDataBinding
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.regex.Pattern
+
 
 class AddDataFragment : Fragment() {
 
@@ -25,6 +32,11 @@ class AddDataFragment : Fragment() {
     private lateinit var viewModel: AddDataViewModel
     private var _binding: FragmentAddDataBinding? = null
     private val binding get() = _binding!!
+    // one boolean variable to check whether all the text fields
+    // are filled by the user, properly or not.
+    private var validateFields = true
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,16 +53,21 @@ class AddDataFragment : Fragment() {
         }
 
         binding.addTransactionButton.setOnClickListener {
-            findNavController().navigate(R.id.action_addDataFragment_to_historyFragment)
+            validateFields = checkAllFields()
+            if (validateFields) {
+                findNavController().navigate(R.id.action_addDataFragment_to_historyFragment)
 
-            var dataList = arrayOf(binding.dateText.text.toString(),
-                binding.moneyText.text.toString(),
-                binding.descriptionText.text.toString(),
-                binding.typeDropdown.selectedItem.toString())
+                var dataList = arrayOf(
+                    binding.dateText.text.toString(),
+                    binding.moneyText.text.toString(),
+                    binding.descriptionText.text.toString(),
+                    binding.typeDropdown.selectedItem.toString()
+                )
 
-            setFragmentResult("requestKey", bundleOf("bundleKey" to dataList))
+                setFragmentResult("requestKey", bundleOf("bundleKey" to dataList))
 
-            hideKeyboard()
+                hideKeyboard()
+            }
         }
 
         val adapter = this.context?.let {
@@ -66,6 +83,35 @@ class AddDataFragment : Fragment() {
         return root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun validate(registerDate: String): Boolean {
+        val p: Pattern = Pattern.compile("^(1[0-9]|0[1-9]|3[0-1]|2[1-9])/(0[1-9]|1[0-2])/[0-9]{4}$")
+        val m = p.matcher(registerDate)
+        if (!m.matches()) return false
+        val format = SimpleDateFormat("dd/MM/yyyy")
+        return try {
+            format.parse(registerDate)
+            true
+        } catch (e: ParseException) {
+            false
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun checkAllFields(): Boolean {
+        var check = true
+        if (binding.dateText.text.isEmpty()) {
+            binding.dateText.error = "The date is empty"
+            check = false
+        } else if (!validate(binding.dateText.text.toString())) {
+            binding.dateText.error = "The date is improperly formatted"
+            check = false
+        } else if (binding.moneyText.text.toString().isEmpty()) {
+            binding.moneyText.error = "The amount is empty"
+            check = false
+        }
+        return check
+    }
 
     fun hideKeyboard() {
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
