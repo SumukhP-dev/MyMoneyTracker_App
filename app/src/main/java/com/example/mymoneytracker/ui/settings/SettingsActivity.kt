911b0152415f4ee866a7195f1d742ff45cecdac3
SumukhP0 <spaspuleti3@gatech.edu>
2024-05-11 18:57:20 -0400

    Added back button in SettingsActivity

--- app/src/main/java/com/example/mymoneytracker/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/mymoneytracker/ui/settings/SettingsActivity.kt
@@ -2,11 +2,24 @@ package com.example.mymoneytracker.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
 import com.example.mymoneytracker.R
+import com.example.mymoneytracker.databinding.ActivityLoginBinding
+import com.example.mymoneytracker.databinding.ActivitySettingsBinding
+import com.example.mymoneytracker.ui.home.HomeFragment
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.constraintLayout5, HomeFragment()).commit()
+            finish()
+        }
     }
 }
