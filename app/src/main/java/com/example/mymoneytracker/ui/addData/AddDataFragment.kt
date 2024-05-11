954eed0490bfff438ca287724ffbe697e4d44bda
SumukhP0 <spaspuleti3@gatech.edu>
2024-05-11 18:34:48 -0400

    Added autocomplete to date for AddDataFragment

--- app/src/main/java/com/example/mymoneytracker/ui/addData/AddDataFragment.kt
+++ app/src/main/java/com/example/mymoneytracker/ui/addData/AddDataFragment.kt
@@ -3,6 +3,8 @@ package com.example.mymoneytracker.ui.addData
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -74,6 +76,20 @@ class AddDataFragment : Fragment() {
         adapter?.setDropDownViewResource(android.R.layout.simple_spinner_item)
         binding.typeDropdown.adapter = adapter
 
+        binding.dateText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.dateText.text.length == 2 || binding.dateText.text.length == 5) {
+                    binding.dateText.setText("${binding.dateText.text}/")
+                    binding.dateText.setSelection(binding.dateText.text.length)
+                }
+            }
+        })
         return root
     }
 
