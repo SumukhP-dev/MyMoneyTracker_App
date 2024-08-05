ca39ed18f2ad466f7ad6ee628f3d329d94636539
SumukhP-dev <156015540+SumukhP-dev@users.noreply.github.com>
2024-08-05 08:40:00 -0400

    Changed tips text to use regular string resources concatenation

--- app/src/main/java/com/example/mymoneytracker/ui/summary/SummaryFragment.kt
+++ app/src/main/java/com/example/mymoneytracker/ui/summary/SummaryFragment.kt
@@ -3,6 +3,7 @@ package com.example.mymoneytracker.ui.summary
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +15,6 @@ import com.example.mymoneytracker.R
 import com.example.mymoneytracker.databinding.FragmentSummaryBinding
 import com.example.mymoneytracker.ui.OnSwipeTouchListener
 import org.eazegraph.lib.models.PieModel
-
 
 class SummaryFragment : Fragment() {
     private lateinit var viewModel: SummaryViewModel
@@ -33,7 +33,7 @@ class SummaryFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(SummaryViewModel::class.java)
 
         // This implements swipe gestures to go to Home Fragment
-        binding.nestedScrollView.setOnTouchListener(object: OnSwipeTouchListener(context) {
+        binding.nestedScrollView.setOnTouchListener(object : OnSwipeTouchListener(context) {
             override fun onSwipeRight() {
                 findNavController().navigate(R.id.action_summaryFragment_to_nav_home)
             }
@@ -47,20 +47,21 @@ class SummaryFragment : Fragment() {
             findNavController().navigate(R.id.action_summaryFragment_to_nav_home)
         }
 
-        viewModel.displayTips(viewModel.user.getNetWorthCalculated())
-        binding.tips.text = viewModel.user.getTipsMessages()
+        displayTips(viewModel.user.getNetWorthCalculated())
+        Log.d("tipstext", viewModel.user.getTipsMessages())
+        binding.tipsText.text = viewModel.user.getTipsMessages()
 
         binding.progressBar.progress = viewModel.getProgress(viewModel.user.getNetWorthCalculated())
 
         initializePieChart()
 
         binding.progressBar.max = 100
-        binding.progressBar.progressTintList = ColorStateList.valueOf(Color.GREEN);
+        binding.progressBar.progressTintList = ColorStateList.valueOf(Color.GREEN)
 
         return root
     }
 
-    fun initializePieChart() {
+    private fun initializePieChart() {
         for (element in viewModel.user.getDataForPieChart()) {
             if (element != 0.0) {
                 setPieChart(viewModel.user.getDataForPieChart())
@@ -70,57 +71,100 @@ class SummaryFragment : Fragment() {
         setPieChartDefault()
     }
 
-    fun setPieChartDefault(){
+    private fun setPieChartDefault() {
         binding.pieChart.addPieSlice(
             PieModel(
-                0F, Color.parseColor("#A9A9A9")
+                0F,
+                Color.parseColor("#A9A9A9")
             )
         )
     }
 
-    fun setPieChart(dataForPieChart: Array<Double>) {
+    private fun setPieChart(dataForPieChart: Array<Double>) {
         // Set the data and color to the pie chart
         binding.pieChart.addPieSlice(
             PieModel(
-                "Rent/Mortgage", dataForPieChart[0].toFloat(),
+                "Rent/Mortgage",
+                dataForPieChart[0].toFloat(),
                 Color.parseColor("#FFA726")
             )
         )
         binding.pieChart.addPieSlice(
             PieModel(
-                "Utilities", dataForPieChart[1].toFloat(),
+                "Utilities",
+                dataForPieChart[1].toFloat(),
                 Color.parseColor("#66BB6A")
             )
         )
         binding.pieChart.addPieSlice(
             PieModel(
-                "Student Loans", dataForPieChart[2].toFloat(),
+                "Student Loans",
+                dataForPieChart[2].toFloat(),
                 Color.parseColor("#EF5350")
             )
         )
         binding.pieChart.addPieSlice(
             PieModel(
-                "Car payments", dataForPieChart[3].toFloat(),
+                "Car payments",
+                dataForPieChart[3].toFloat(),
                 Color.parseColor("#29B6F6")
             )
         )
         binding.pieChart.addPieSlice(
             PieModel(
-                "Food", dataForPieChart[4].toFloat(),
+                "Food",
+                dataForPieChart[4].toFloat(),
                 Color.parseColor("#673AB7")
             )
         )
         binding.pieChart.addPieSlice(
             PieModel(
-                "Fun", dataForPieChart[5].toFloat(),
+                "Fun",
+                dataForPieChart[5].toFloat(),
                 Color.parseColor("#009688")
             )
         )
         binding.pieChart.addPieSlice(
             PieModel(
-                "Miscellaneous", dataForPieChart[6].toFloat(),
+                "Miscellaneous",
+                dataForPieChart[6].toFloat(),
                 Color.parseColor("#3F51B5")
             )
         )
+    }
+
+    // This function displays tips based on net worth
+    fun displayTips(netWorth: Double) {
+        val baseText: String = resources.getString(R.string.tip6) + "\n" +
+            resources.getString(R.string.tip7) + "\n" +
+            resources.getString(R.string.tip8) + "\n" +
+            resources.getString(R.string.tip9)
+
+        if (netWorth >= 100000) {
+            viewModel.user.setTipsMessages(
+                resources.getString(R.string.tip1) + "\n" +
+                    baseText
+            )
+        } else if (netWorth >= 10000) {
+            viewModel.user.setTipsMessages(
+                resources.getString(R.string.tip2) + "\n" +
+                    baseText
+            )
+        } else if (netWorth >= 1000) {
+            viewModel.user.setTipsMessages(
+                resources.getString(R.string.tip3) + "\n" +
+                    baseText
+            )
+        } else if (netWorth >= -1000) {
+            viewModel.user.setTipsMessages(
+                resources.getString(R.string.tip4) + "\n" +
+                    baseText
+            )
+        } else {
+            viewModel.user.setTipsMessages(
+                resources.getString(R.string.tip5) + "\n" +
+                    baseText
+            )
+        }
     }
 }
--- app/src/main/java/com/example/mymoneytracker/ui/summary/SummaryViewModel.kt
+++ app/src/main/java/com/example/mymoneytracker/ui/summary/SummaryViewModel.kt
@@ -1,61 +1,12 @@
 package com.example.mymoneytracker.ui.summary
 
+import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import com.example.mymoneytracker.R
 import com.example.mymoneytracker.model.User
 
 class SummaryViewModel : ViewModel() {
     var user: User = User.getInstance()
-
-    // This function displays tips based on net worth
-    fun displayTips(netWorth: Double) {
-        if (netWorth >= 100000) {
-            user.setTipsMessages(UiText.StringResources(
-                resId = R.string.tipBase,
-                R.string.tip1,
-                R.string.tip6,
-                R.string.tip7,
-                R.string.tip8,
-                R.string.tip9
-            ).toString())
-        } else if (netWorth >= 10000) {
-            user.setTipsMessages(UiText.StringResources(
-                resId = R.string.tipBase,
-                R.string.tip2,
-                R.string.tip6,
-                R.string.tip7,
-                R.string.tip8,
-                R.string.tip9
-            ).toString())
-        } else if (netWorth >= 1000) {
-            user.setTipsMessages(UiText.StringResources(
-                resId = R.string.tipBase,
-                R.string.tip3,
-                R.string.tip6,
-                R.string.tip7,
-                R.string.tip8,
-                R.string.tip9
-            ).toString())
-        } else if (netWorth >= -1000) {
-            user.setTipsMessages(UiText.StringResources(
-                resId = R.string.tipBase,
-                R.string.tip4,
-                R.string.tip6,
-                R.string.tip7,
-                R.string.tip8,
-                R.string.tip9
-            ).toString())
-        } else {
-            user.setTipsMessages(UiText.StringResources(
-                resId = R.string.tipBase,
-                R.string.tip5,
-                R.string.tip6,
-                R.string.tip7,
-                R.string.tip8,
-                R.string.tip9
-            ).toString())
-        }
-    }
 
     // This function gets the amount of progress for the progress bar
     fun getProgress(netWorth: Double): Int {
