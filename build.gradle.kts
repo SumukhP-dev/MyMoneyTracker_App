f5d574922047a9784d826f58d0c860494349c46d
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-08-20 03:01:31 -0400

    Add requirements.txt file

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.8.2" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.3"
     id ("com.google.gms.google-services") version "4.4.2" apply false
